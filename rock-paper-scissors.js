ac151ae3c0d2daf127a35b4e963736c05ccc6f76
Alex Swan <alex.swan1991@gmail.com>
2021-02-20 21:29:54 +0000

    Corrected some functions and added better puncution and grammar to deciding message. THIS WORKS AS ONE ROUND OF ROCK PAPER SCISSORS

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,8 +1,12 @@
-// const playerSelection = prompt(`Lets play a game of Rock Paper Scissors!
-//   Which option do you choose?`);
 // const lostMessage = `You lost, ${computerPlay()} beats ${playerSelection}`;
 // const winMessage = `You won! ${playerSelection} beats ${computerPlay()}`;
 // const drawMessage = `You both draw, you both picked ${playerSelection}`;
+
+let playerSelection = prompt(
+  "Lets play a game of Rock Paper Scissors! Which option do you choose?"
+);
+playerSelection = playerSelection.toUpperCase();
+const computerSelection = computerPlay();
 
 function computerPlay() {
   const computerOption = Math.floor(Math.random() * 3) + 1;
@@ -16,24 +20,25 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === "ROCK" && computerSelection ===== "PAPER") {
+  if (playerSelection == "ROCK" && computerSelection == "PAPER") {
     return "You lose! Paper beats rock";
   } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
-      return "You win! Rock beats paper";
+    return "You win! Rock beats paper";
   } else if (playerSelection === "ROCK" && computerSelection === "ROCK") {
-      return "You draw! You both picked ROCK";
-  }   else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
-    return "You lose! Scissors beats paper";
+    return "You draw! You both picked rock";
+  } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
+    return "You lose! Scissors beats PAPER";
   } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
-      return "You win! PAPER beats ROCK";
+    return "You win! Paper beats rock";
   } else if (playerSelection === "PAPER" && computerSelection === "PAPER") {
-      return "You draw! You both picked PAPER";
-  }  else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+    return "You draw! You both picked paper";
+  } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
     return "You lose! Scissors beats paper";
   } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
-      return "You win! SCISSORS beats PAPER";
-  } else if (playerSelection === "SCISSORS" && computerSelection === "SCISSORS") {
-      return "You draw! You both picked SCISSORS";
+    return "You win! Scissors beats paper";
+  } else {
+    return "You draw! You both picked SCISSORS";
   }
+}
 
-  const playerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
