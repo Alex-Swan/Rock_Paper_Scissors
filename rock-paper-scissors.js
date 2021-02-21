a8ef9537ed98c5850b8c9a25721020d01f9a23c4
Alex Swan <alex.swan1991@gmail.com>
2021-02-21 17:32:01 +0000

    Issue with computer choices fixed so that it produces a random choice each time, also added a point system so that at the end of the 5 rounds a message is produced informing user who is the winner or if it was a draw

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,9 +1,10 @@
+// Variables
 let playerSelection;
 let playerScore = 0;
 let computerScore = 0;
+let computerSelection = computerPlay();
 
-const computerSelection = computerPlay();
-
+// This function below randomly chooses between ROCK PAPER or SCISSORS
 function computerPlay() {
   const computerOption = Math.floor(Math.random() * 3) + 1;
   if (computerOption === 1) {
@@ -15,22 +16,33 @@ function computerPlay() {
   }
 }
 
+/* 
+This function below sees what the player input is and what the random computer option, it then adds 1 to the winners points
+after this it posts to the console who won that round or it was a draw. It also posts an error message if player input is
+not recognized 
+*/
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == "ROCK" && computerSelection == "PAPER") {
+    computerScore = computerScore += 1;
     return "You lose! Paper beats rock";
   } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+    playerScore = playerScore += 1;
     return "You win! Rock beats paper";
   } else if (playerSelection === "ROCK" && computerSelection === "ROCK") {
     return "You draw! You both picked rock";
   } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
+    computerScore = computerScore += 1;
     return "You lose! Scissors beats PAPER";
   } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+    playerScore = playerScore += 1;
     return "You win! Paper beats rock";
   } else if (playerSelection === "PAPER" && computerSelection === "PAPER") {
     return "You draw! You both picked paper";
   } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+    computerScore = computerScore += 1;
     return "You lose! Scissors beats paper";
   } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+    playerScore = playerScore += 1;
     return "You win! Scissors beats paper";
   } else if (
     playerSelection === "SCISSORS" &&
@@ -41,42 +53,64 @@ function playRound(playerSelection, computerSelection) {
     return 'There was an error. Please type "Rock" "Paper" or "Scissors". ';
   }
 }
-
+/* 
+This function below plays the game 5 times and at the end posts who the winner is of the 5 rounds, it can also post a draw message.
+*/
 function game() {
   playerSelection = prompt(
     "Lets play a game of Rock Paper Scissors! Which option do you choose?"
   ).toUpperCase();
-  computerPlay();
+  computerSelection = computerPlay();
   playRound(playerSelection, computerSelection);
   console.log(playRound(playerSelection, computerSelection));
 
   playerSelection = prompt(
     "Lets play a game of Rock Paper Scissors! Which option do you choose?"
   ).toUpperCase();
-  computerPlay();
+  computerSelection = computerPlay();
   playRound(playerSelection, computerSelection);
   console.log(playRound(playerSelection, computerSelection));
 
   playerSelection = prompt(
     "Lets play a game of Rock Paper Scissors! Which option do you choose?"
   ).toUpperCase();
-  computerPlay();
+  computerSelection = computerPlay();
   playRound(playerSelection, computerSelection);
   console.log(playRound(playerSelection, computerSelection));
 
   playerSelection = prompt(
     "Lets play a game of Rock Paper Scissors! Which option do you choose?"
   ).toUpperCase();
-  computerPlay();
+  computerSelection = computerPlay();
   playRound(playerSelection, computerSelection);
   console.log(playRound(playerSelection, computerSelection));
 
   playerSelection = prompt(
     "Lets play a game of Rock Paper Scissors! Which option do you choose?"
   ).toUpperCase();
-  computerPlay();
+  computerSelection = computerPlay();
   playRound(playerSelection, computerSelection);
   console.log(playRound(playerSelection, computerSelection));
+
+  if (playerScore > computerScore) {
+    console.log(
+      `Congratulations!! You win, you got ${
+        playerScore / 2
+      } and the computer got ${computerScore / 2}!`
+    );
+  } else if (playerScore < computerScore) {
+    console.log(
+      `Unfortunately you lost! You got ${
+        playerScore / 2
+      } and the computer got ${computerScore / 2}. Bad luck! `
+    );
+  } else if (playerScore === computerScore) {
+    console.log(
+      `You both drew! Wowzer! You both got ${
+        playerScore / 2
+      }. How about another go?`
+    );
+  }
 }
 
 game();
